e5b4e518339df57637948f9660c40879718d3b74
Taha <tahasiddiqui31@gmail.com>
2021-01-16 04:57:33 -0500

    Initial commit

---
+++ app/src/main/java/com/siddiqui/recommendations/android/MainActivity.java
@@ -0,0 +1,52 @@
+package com.siddiqui.recommendations.android;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.FirebaseFirestore;
+import com.siddiqui.recommendations.R;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FirebaseDatabase database = FirebaseDatabase.getInstance();
+        DatabaseReference myRef = database.getReference("message");
+        myRef.setValue("hello, world");
+
+        myRef.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                String value = snapshot.getValue(String.class);
+                Log.d("Tag","Value is "+value);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                Log.w("TAG","Failed to read value",error.toException());
+            }
+        });
+
+    }
+
+
+}
---
+++ app/src/main/java/com/siddiqui/recommendations/database/DatabaseBuilder.java
@@ -0,0 +1,6 @@
+package com.siddiqui.recommendations.database;
+
+// Created by Taha Siddiqui
+// 16/01/2021
+public class DatabaseBuilder {
+}
