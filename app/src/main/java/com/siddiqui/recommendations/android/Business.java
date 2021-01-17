148e147acf50bc2b2e844700f27e31d7b47d231c
Calvin Liu <66889924+liucalvin@users.noreply.github.com>
2021-01-16 17:23:42 -0700

    Complete business list ui (#1)
    * Complete business list ui
    * Add Business Detail Screen
    * Finish registration screen

---
+++ app/src/main/java/com/siddiqui/recommendations/android/Business.java
@@ -0,0 +1,37 @@
+package com.siddiqui.recommendations.android;
+
+public class Business {
+
+    private String name, address;
+    private Long id;
+
+    public Business(String name, String address, Long id) {
+        this.name = name;
+        this.address = address;
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+}
--- app/src/main/java/com/siddiqui/recommendations/android/MainActivity.java
+++ app/src/main/java/com/siddiqui/recommendations/android/MainActivity.java
@@ -1,30 +1,18 @@
 package com.siddiqui.recommendations.android;
 
 import android.os.Bundle;
-import android.util.Log;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.android.gms.tasks.Task;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.database.DataSnapshot;
-import com.google.firebase.database.DatabaseError;
-import com.google.firebase.database.DatabaseReference;
-import com.google.firebase.database.FirebaseDatabase;
-import com.google.firebase.database.ValueEventListener;
 import com.google.firebase.firestore.DocumentReference;
 import com.google.firebase.firestore.FirebaseFirestore;
-import com.google.firebase.firestore.FirebaseFirestoreSettings;
 import com.google.firebase.firestore.QueryDocumentSnapshot;
 import com.google.firebase.firestore.QuerySnapshot;
 import com.siddiqui.recommendations.R;
-
-import java.util.HashMap;
-import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -118,6 +106,4 @@ public class MainActivity extends AppCompatActivity {
                     }
                 });
     }
-
-
 }
